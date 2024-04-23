831cb104e0781e7f6fe3938df227ad90b9182d5d
Cassian Godsted <cassian@godsted.com>
2024-04-23 13:40:14 -0400

    Begin 0.9 backport

---
+++ src/main/java/cc/cassian/pyrite/ModButton.java
@@ -0,0 +1,9 @@
+package cc.cassian.pyrite;
+
+import net.minecraft.block.WoodenButtonBlock;
+
+public class ModButton extends WoodenButtonBlock {
+    public ModButton(Settings settings) {
+        super(settings);
+    }
+}
--- src/main/java/cc/cassian/pyrite/ModGlass.java
+++ src/main/java/cc/cassian/pyrite/ModGlass.java
@@ -4,6 +4,6 @@ import net.minecraft.block.GlassBlock;
 import net.minecraft.block.Material;
 
 public class ModGlass extends GlassBlock {
-    public ModGlass() {
-        super(Settings.of(Material.GLASS).nonOpaque().strength(2.0f)); }
+    public ModGlass(Settings settings) {
+        super(settings); }
 }
