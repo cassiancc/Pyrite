795c0b6d736f10d002393e20e5b54bd3a43eb058
Cassian Godsted <cassian@godsted.com>
2024-05-28 12:57:03 -0400

    Update PyriteClient.java

--- fabric/src/main/java/cc/cassian/pyrite/fabric/client/PyriteClient.java
+++ fabric/src/main/java/cc/cassian/pyrite/fabric/client/PyriteClient.java
@@ -1,17 +1,14 @@
 package cc.cassian.pyrite.fabric.client;
 
-
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.BiomeColors;
-import net.minecraft.world.level.block.Block;
-
-import static cc.cassian.pyrite.functions.ModHelpers.grassBlocks;
-import static cc.cassian.pyrite.functions.ModHelpers.transparentBlocks;
+import net.minecraft.block.*;
+import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.client.render.RenderLayer;
+import static cc.cassian.pyrite.functions.ModHelpers.*;
 
 
 @Environment(EnvType.CLIENT)
@@ -19,10 +16,10 @@ public class PyriteClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         for (Block pyriteBlock : grassBlocks) {
-            ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> BiomeColors.getAverageGrassColor(view, pos), pyriteBlock);
+            ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> BiomeColors.getGrassColor(view, pos), pyriteBlock);
             ColorProviderRegistry.ITEM.register((stack, tintIndex) -> 7183658, pyriteBlock);
         }
         for (Block pyriteBlock : transparentBlocks) {
-            BlockRenderLayerMap.INSTANCE.putBlock(pyriteBlock, RenderType.cutout());
+            BlockRenderLayerMap.INSTANCE.putBlock(pyriteBlock, RenderLayer.getCutout());
         }}
 }
