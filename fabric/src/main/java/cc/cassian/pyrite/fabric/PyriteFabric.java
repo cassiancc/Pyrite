df094f87dbcc633969d5177266ffe773aab41b2b
Cassian Godsted <cassian@godsted.com>
2024-05-29 13:46:09 -0400

    [Fabric] Remove Fabric-specific fuel registry

--- fabric/src/main/java/cc/cassian/pyrite/fabric/PyriteFabric.java
+++ fabric/src/main/java/cc/cassian/pyrite/fabric/PyriteFabric.java
@@ -2,14 +2,10 @@ package cc.cassian.pyrite.fabric;
 
 import cc.cassian.pyrite.Pyrite;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.registry.FuelRegistry;
-
-import static cc.cassian.pyrite.functions.ModHelpers.fuel;
 
 public class PyriteFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         Pyrite.init();
-        fuel.forEach((fuelBlock, fuelLength) -> FuelRegistry.INSTANCE.add(fuelBlock.get(), fuelLength));
     }
 }
