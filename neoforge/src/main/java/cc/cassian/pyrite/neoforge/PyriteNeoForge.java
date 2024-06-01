d2b4bff8a365b62a7459f369d37c2a53b7018873
Cassian Godsted <cassian@godsted.com>
2024-06-01 01:07:37 -0400

    More 0.13 fixes

--- neoforge/src/main/java/cc/cassian/pyrite/neoforge/PyriteNeoForge.java
+++ neoforge/src/main/java/cc/cassian/pyrite/neoforge/PyriteNeoForge.java
@@ -2,14 +2,14 @@ package cc.cassian.pyrite.neoforge;
 
 
 import cc.cassian.pyrite.Pyrite;
-import net.neoforged.bus.api.IEventBus;
+import net.minecraft.SharedConstants;
 import net.neoforged.fml.common.Mod;
 
 @Mod(Pyrite.modID)
 public final class PyriteNeoForge {
     public PyriteNeoForge() {
         // Run our common setup.
-        Pyrite.init("neoforge");
+        Pyrite.init(SharedConstants.WORLD_VERSION + "-neoforge");
 
     }
 }
