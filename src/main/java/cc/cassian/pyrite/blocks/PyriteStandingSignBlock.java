472609c70b900551fc52e941dc83810da324b8a9
Cassian <cassian@godsted.com>
2026-05-11 13:39:46 -0400

    Fix signs not being registered correctly

---
+++ src/main/java/cc/cassian/pyrite/blocks/PyriteStandingSignBlock.java
@@ -0,0 +1,21 @@
+package cc.cassian.pyrite.blocks;
+
+import net.minecraft.world.level.block.StandingSignBlock;
+import net.minecraft.world.level.block.WallSignBlock;
+import net.minecraft.world.level.block.state.properties.WoodType;
+
+public class PyriteStandingSignBlock extends StandingSignBlock {
+    private final String id;
+
+    public PyriteStandingSignBlock(WoodType type, Properties properties, String blockID) {
+        super(type, properties);
+        this.id = blockID;
+    }
+
+    //? if <1.21.2 {
+    /*@Override
+    public String getDescriptionId() {
+        return Pyrite.of(id).toLanguageKey("block");
+    }
+    *///?}
+}
