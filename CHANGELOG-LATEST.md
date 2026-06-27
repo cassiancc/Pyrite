018735a3f32ff0fb0618f574d202c18e3ef1ceb5
Cassian <cassian@godsted.com>
2026-06-27 15:05:01 -0400

    Fix grass turf tinting

--- CHANGELOG-LATEST.md
+++ CHANGELOG-LATEST.md
@@ -2,3 +2,4 @@
 - Wool Stairs and Slabs are now mined faster with Shears.
 - Wool Stairs and Slabs now dampen vibrations.
 - Resource conditions now work properly on NeoForge.
+- Grass Turf Slabs are now tinted correctly.
--- src/main/java/cc/cassian/pyrite/client/PyriteClient.java
+++ src/main/java/cc/cassian/pyrite/client/PyriteClient.java
@@ -6,11 +6,17 @@ import cc.cassian.pyrite.util.ModHelpers;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.color.block.BlockTintSource;
 import net.minecraft.client.color.block.BlockTintSources;
+import net.minecraft.client.renderer.BiomeColors;
+import net.minecraft.client.renderer.block.BlockAndTintGetter;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.GrassColor;
+import net.minecraft.world.level.block.state.BlockState;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -19,7 +25,19 @@ import static cc.cassian.pyrite.Pyrite.MOD_ID;
 public class PyriteClient {
 
 	public static List<BlockTintSource> registerColor() {
-		return Collections.singletonList(BlockTintSources.grassBlock());
+		return Collections.singletonList(new BlockTintSource() {
+			public int color(final BlockState state) {
+				return GrassColor.getDefaultColor();
+			}
+
+			public int colorInWorld(final BlockState state, final BlockAndTintGetter level, final BlockPos pos) {
+				return BiomeColors.getAverageGrassColor(level, pos);
+			}
+
+			public int colorAsTerrainParticle(final BlockState state, final BlockAndTintGetter level, final BlockPos pos) {
+				return GrassColor.getDefaultColor();
+			}
+		});
 	}
 
 	public static void addTooltip(List<Component> lines, ItemStack stack) {
